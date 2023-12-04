d2c665e4c4639be23ce01494aee355cb782a6f1b
yeiad <kabidyeiad101@gmail.com>
2023-12-04 17:28:09 +0600

    added

--- Module 1/Assignments/p1.py
+++ Module 1/Assignments/p1.py
@@ -1,1 +1,11 @@
-print('hello')
+def factorial(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return n * factorial(n - 1)
+
+
+print(factorial(5))  
+print(factorial(4))  
+
+#The time complexity is O(n) since the function calls itself recursively n times.
